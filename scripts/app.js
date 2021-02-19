6a7ac68dc6fccd06efee44eef158602587ca8360
jsolano <justinesolano2370@gmail.com>
2021-02-19 15:20:50 +0000

    starting grid creation

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,38 @@
 function init(){
 
   console.log('CONTENT LOADED')
+  
+  // VARIABLES
+  const grid = document.querySelector('.grid')
+  const width = 11
+  const cellCount = width * 11
+  const cells = []
+  const frodoClass = 'frodo'
+
+  const startPosition = 0
+  console.log(startPosition)
+  
+  let currentPosition = 0
+
+
+  // GRID CREATION
+  function createGrid (startPosition) {
+    for (let i = 0; i < cellCount; i++) {
+
+      const cell = document.createElement('div')
+      cell.innerText = i
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+    addFrodo(startPosition)
+  }
+
+  // ADD CAT TO GRID
+  function addFrodo(position) {
+    console.log(position)
+    console.log(cells[position])
+    cells[position].classList.add(frodoClass)
+  }
 
 
 
@@ -8,7 +40,12 @@ function init(){
 
 
 
+
+
+
+ 
 }
+
 
 window.addEventListener('DOMContentLoaded', init)
 
