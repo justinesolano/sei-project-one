9d47397c9335bbc03f8155bb2824c97495f30109
jsolano <justinesolano2370@gmail.com>
2021-02-19 10:34:13 +0000

    notes after sign off

---
+++ pseudo-code.js
@@ -0,0 +1,68 @@
+// FROGGER
+  // steps towards mvp
+    // javascript grid
+    // 10 x 11 grid (to center player starting point)
+    // alter grid conditionals for arrow movements
+//------------------------
+    // what is your name option? 
+    // <form>
+//------------------------
+    // start button to trigger game
+        // starts whole game
+        // function within function?
+//------------------------
+    // lives? 3-4 (?)
+        // if currentLives => 1, else game over 
+//------------------------
+    // player start point - 6th grid on the bottom row, center
+        // appears onload, not after pressing start button
+        // image on css
+//------------------------
+    // <div> how to play <div> - hover over, instructions show 
+        // Get player to the top of the screen whilst avoiding the traps and water etc
+        // time limit? 30 secs (if timer =<1)
+//------------------------
+    // game over
+        // when landing on specific grids without "grass" and boats
+        // game over alert
+        // restart/reset option
+            // function -> reset player to starting position, eventListener click
+        // also a you win alert (if player is on certain grid div, else game over)
+
+    //    _______________________
+    //    |_|~|_|~|_|~|_|~|_|~|_| final destination
+    //    |~ () ~ () ~ () ~ () ~| water with boats
+    //    |_|@|_|@|_|@|_|@|_|@|_| obstacle
+    //    |                     | land
+    //    |_____________________| land
+    //    |@|_|@|_|@|_|@|_|@|_|@| obstacle
+    //    |_|@|_|@|_|@|_|@|_|@|_| obstacle
+    //    |@|_|@|_|@|_|@|_|@|_|@| obstacle
+    //    |_____________________| land
+    //    |~ () ~ () ~ () ~ () ~| water with boats
+    //    |_|_|_|_|_|P|_|_|_|_|_| starting row
+
+
+    // moving frog
+       // whole function for arrow movements, use conditionals
+
+
+    // how to move obstacles across the grid?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
