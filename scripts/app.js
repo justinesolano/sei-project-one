e3b37573a86466935d710d0f5da639bc3bc220dc
jsolano <justinesolano2370@gmail.com>
2021-02-20 17:10:28 +0000

    working out how to add class to a group of cells to make obstacles

--- scripts/app.js
+++ scripts/app.js
@@ -28,17 +28,31 @@ function init(){
       cell.innerText = i
       grid.appendChild(cell)
       cells.push(cell)
-    //   const rowColumns = document.querySelector('.grid div')
+    //   cell.classList.add([i]) // adding class to each cell div to access single cell and put bg image such as water or boat
+      //   const rowColumns = document.querySelector('.grid div')
     }
     addFrodo(startPosition)
   }
 
   createGrid(startPosition)
 
+  const river = cells.slice(88, 98)
+  river.classList.add('river')
+
   // ADD FRODO TO GRID
   function addFrodo(position) {
     cells[position].classList.add(frodoClass) // this adds the frodoClass of 'frodo' to the cells position by adding frodo to grid div so this is where Frodo will show up
-  }
+  } 
+  
+//   const frog = document.createElement('div')
+//   const lily = document.createElement('div')
+
+//   cells[cells.length - 1].appendChild(frog)
+//   frog.classList.add('frog')
+//   cells[0].appendChild(lily)
+//   lily.classList.add('lily')
+
+
 
   //   function hideText()
   //   {
@@ -65,8 +79,6 @@ function init(){
 
   hoverImg.addEventListener('mouseenter', handleMouseEnter)
   hoverImg.addEventListener('mouseleave', handleMouseLeave)
-
-
 
   // START GAME
   const startButton = document.querySelector('#begin')
