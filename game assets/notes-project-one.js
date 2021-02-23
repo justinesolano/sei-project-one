273440c12d96cd97fb484014f36758bdc249c469
jsolano <justinesolano2370@gmail.com>
2021-02-23 21:00:35 +0000

    working on nazgul movement

--- game assets/notes-project-one.js
+++ game assets/notes-project-one.js
@@ -49,15 +49,39 @@
     // rafts
 
 // ADD POSITION OF ASSET
+    // black riders
+    // trees - frodo cant go into these cells
     // 
 
 
 // MOVEMENT OF ASSET
+    // black riders
 
 
+// NARRATIVE
+    // Road
+    // Black riders
+    // Trees
+    // Black Riders
+    // Rafts
+    // Road
+    // Tavern
 
 
+// ADD FRODO TO GRID
+    // this adds the frodoClass of 'frodo' to the cells position by adding frodo to grid div so this is where Frodo will show up
+    // document.querySelector('.frodo').style.zIndex = '1'
 
+
+// middle - simple every second in timer remove log and add to next one on a timer add 10, remove ,add,c ounter, conditional
+
+// RESTART FUNCTION
+
+//   const restart = document.getElementById('reset')
+//   function resetGame(){
+//     cells[position].classList.remove(frodoClass)
+//     cells[position].classList.add(frodoClass) // this adds the frodoClass of 'frodo' to the cells position by adding frodo to grid div so this is where Frodo will show up
+//   }
 
 
 
